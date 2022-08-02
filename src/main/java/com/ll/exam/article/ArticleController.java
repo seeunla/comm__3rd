d9684547d88a21465f116c1834bde5c85837dd09
seeunla <ksu98712@gmail.com>
2022-08-02 14:26:46 +0900

    객체 생성시 의존성 자동 해결 테스트 케이스 추가

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -1,16 +1,22 @@
 package com.ll.exam.article;
 
+import com.ll.exam.annotation.Autowired;
 import com.ll.exam.annotation.Controller;
 import com.ll.exam.annotation.GetMapping;
 import com.ll.exam.article.service.ArticleService;
 
 @Controller //ArticleController 가 컨트롤러 이다.
 public class ArticleController {
+    @Autowired
     private ArticleService articleService;
 
     @GetMapping("/usr/article/list")
     // 아래 showList 는 Get /usr/article/list 으로 요청이 왔을 때 실행 되어야 하는 함수이다.
     public void showList() {
 
+    }
+
+    public ArticleService getArticleServiceForTest() {
+        return articleService;
     }
 }
