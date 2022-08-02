ace4f86ff78c85bcce9f0ffa5c978b9519bbd473
seeunla <ksu98712@gmail.com>
2022-08-02 13:02:45 +0900

    컴포넌트 스캔

--- src/main/java/com/ll/exam/article/Container.java
+++ src/main/java/com/ll/exam/article/Container.java
@@ -1,7 +1,12 @@
 package com.ll.exam.article;
 
+import com.ll.exam.annotation.Controller;
+import com.ll.exam.Ut;
+import com.ll.exam.article.ArticleController;
 import javassist.tools.reflect.Reflection;
+import org.reflections.Reflections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Container {
@@ -14,7 +19,17 @@ public class Container {
     }
 
     public static List<String> getControllerNames() {
+        List<String> names = new ArrayList<>();
 
+        Reflections ref = new Reflections("com.ll.exam");
+        for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)) {
+            String name = cls.getSimpleName(); // HomeController
+            name = name.replace("Controller", ""); // Home
+            name = Ut.str.decapitalize(name); // home
 
+            names.add(name.replace("Controller", name));
+        }
+
+        return names;
     }
 }
